ed39edd6665687c480fe028c65c00e5c6ad8b290
Sakshi Rastogi <80481744+SakshiRastogi1302@users.noreply.github.com>
2025-09-28 16:03:30 +0530

    updated the structure of header component and added font awesome icon for cart.

--- frontend/src/components/HeaderComponent.jsx
+++ frontend/src/components/HeaderComponent.jsx
@@ -18,25 +18,27 @@ const HeaderComponent = () => {
 
 
   return (
-    <div>
-      <header className='bg-white border-b-2 border-solid border-[#e72929]'>
-        <div className='flex justify-between'>
-          <img src={bellynjoyLogo} alt='BellynJoy Logo' className='w-28 h-16'></img>
-          <ul className='flex items-center pr-2'>
-            <li className='mr-2'>
-              <select className='cursor-pointer'>
-                <option value="name">{user.name}</option>
-                <option value="profile">Profile</option>
-                <option value="orders">Orders</option>
-                <option value="logout">Logout</option>
-              </select>
-            </li>
-            <li className='mr-[2px]'>Cart</li>
-            <li className='w-6 h-6 bg-[#e72929] text-white rounded-full flex justify-center'>{cart.totalCount}</li>
-          </ul>
-        </div>
-      </header>
-    </div>
+    <header className='bg-white border-b-2 border-solid border-[#e72929]'>
+      <nav className='flex justify-between'>
+        <Link to="/"><img src={bellynjoyLogo} alt='BellynJoy Logo' className='w-28 h-16'></img></Link>
+        <ul className='flex items-center space-x-4 px-2'>
+          <li>
+            <select className='cursor-pointer border rounded px-2 py-1'>
+              <option value="name">{user.name}</option>
+              <option value="profile">Profile</option>
+              <option value="orders">Orders</option>
+              <option value="logout">Logout</option>
+            </select>
+          </li>
+          <li>
+            <Link to='/' className='flex space-x-1'>
+              <span><i class="fa-solid fa-cart-shopping"></i>Cart</span> 
+              <span className='w-6 h-6 bg-[#e72929] text-white rounded-full flex justify-center'>{cart.totalCount}</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
