15a4ed027314fdb62cc597bfabfb5e318224bbe2
Sakshi Rastogi <80481744+SakshiRastogi1302@users.noreply.github.com>
2025-10-14 12:03:47 +0530

    added methods

--- frontend/src/services/foodService.js
+++ frontend/src/services/foodService.js
@@ -1,3 +1,14 @@
-import { sample_foods } from "../utils/data"
+import { sample_foods, sample_tags } from "../utils/data"
 
 export const getAllFoodItems = async() => sample_foods;
+
+export const search = async (searchTerm) => sample_foods.filter((items) => items.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
+export const getAllTags = async() => sample_tags;
+
+export const getAllByTags = async(tag) => {
+    if(tag==="All") return getAllFoodItems();
+    return sample_foods.filter((items) => items.tags?.includes(tag));
+}
+
+export const getById = async(foodId) => sample_foods.find((items) => items.id === foodId); 
